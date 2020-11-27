fd5fb4c6ca98dc5d05ff3ea482aad3755352b818
venkatkumari <venki.kumari@gmail.com>
2020-11-27 15:48:42 -0500

    About Section

---
+++ capture/src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+//page components
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
