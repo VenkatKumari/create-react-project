56bfbdf16e21b001dce9b13fedcf915b46dee516
venkatkumari <venki.kumari@gmail.com>
2020-11-27 19:33:04 -0500

    React Router

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
-
+//Import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
-
-//Import Pages
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          {" "}
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
